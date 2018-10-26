88203aa6df9a994b8966b382ac3ff8f07948ce04
BolajiOlajide <bolaji.olajide@andela.com>
2018-10-26 01:42:36 +0100

    fix cron job settings

--- cron.py
+++ cron.py
@@ -1,10 +1,12 @@
 import schedule
 import time
 
+
 def job():
     print("I'm working...")
 
-schedule.every(1).minutes.do(job)
+
+schedule.every(1).seconds.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
 # schedule.every().monday.do(job)
@@ -14,7 +16,7 @@ while True:
     try:
         print('starting work')
         schedule.run_pending()
-        time.sleep(100)
+        time.sleep(60)
     except KeyboardInterrupt:
         print('The user stopped my operation')
         exit()
