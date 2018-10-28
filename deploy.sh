fab4236f1e7c69db2dd2a46e97a7a24ebaddcd5d
BolajiOlajide <bolaji.olajide@andela.com>
2018-10-28 03:10:50 +0100

    allow heroku auto install dependencies

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,9 @@ set -o pipefail # if any code doesn't return 0, exit the script
 echo 'Running deploy scripts for Ceefive bot!'
 
 # install python & project dependencies
-pip install pipenv
-pipenv install
+# no need to do this ha HEroku supports pipenv lol 😂
+# pip install pipenv
+# pipenv install
 
 # starting the bot 😎
 echo '😂 💩 😂 😎 😜 ........ 🧐 🤓 😏'
